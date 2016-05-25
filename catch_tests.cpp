4bea7414a386688cfd44be85bc4398b366e6f586
Dmitry Ledentsov <dmlled@yahoo.com>
2016-05-25 12:13:32 +0200

    test as an example for #21

--- catch_tests.cpp
+++ catch_tests.cpp
@@ -12,59 +12,59 @@ using namespace std;
 
 
 class B{
-	friend class hiberlite::access;
-	template<class Archive>
-	void hibernate(Archive & ar)
-	{
-		ar & HIBERLITE_NVP(name);
-		ar & HIBERLITE_NVP(weight);
-		ar & HIBERLITE_NVP(height);
-	}
+    friend class hiberlite::access;
+    template<class Archive>
+    void hibernate(Archive & ar)
+    {
+        ar & HIBERLITE_NVP(name);
+        ar & HIBERLITE_NVP(weight);
+        ar & HIBERLITE_NVP(height);
+    }
 
-	public:
-		string name;
-		double weight;
-		int height;
+    public:
+        string name;
+        double weight;
+        int height;
 };
 
 class A{
-	friend class hiberlite::access;
-	template<class Archive>
-	void hibernate(Archive & ar)
-	{
-		ar & HIBERLITE_NVP(seti);
-		ar & HIBERLITE_NVP(i);
-		ar & HIBERLITE_NVP(s);
-		ar & HIBERLITE_NVP(vi);
-		ar & HIBERLITE_NVP(v);
-		ar & HIBERLITE_NVP(mm);
-	}
+    friend class hiberlite::access;
+    template<class Archive>
+    void hibernate(Archive & ar)
+    {
+        ar & HIBERLITE_NVP(seti);
+        ar & HIBERLITE_NVP(i);
+        ar & HIBERLITE_NVP(s);
+        ar & HIBERLITE_NVP(vi);
+        ar & HIBERLITE_NVP(v);
+        ar & HIBERLITE_NVP(mm);
+    }
 
-	public:
-		int i;
-		string s;
-		vector< pair<int,int> > vi;
-		set< double > seti;
-		vector< bean_ptr<B> > v;
-		map<int,vector<int> > mm;
+    public:
+        int i;
+        string s;
+        vector< pair<int,int> > vi;
+        set< double > seti;
+        vector< bean_ptr<B> > v;
+        map<int,vector<int> > mm;
 };
 
 class X{
-	friend class hiberlite::access;
-	template<class Archive>
-	void hibernate(Archive & ar)
-	{
-		ar & HIBERLITE_NVP(seti);
-		ar & HIBERLITE_NVP(vvi);
-		ar & HIBERLITE_NVP(mm);
-	}
-	public:
-		set< pair<int,double> > seti;
-		vector< vector<int> >  vvi;
-		map< vector<string>, map<string,string> > mm;
-		bool operator==(const X& x){
-			return seti==x.seti && vvi==x.vvi && mm==x.mm;
-		}
+    friend class hiberlite::access;
+    template<class Archive>
+    void hibernate(Archive & ar)
+    {
+        ar & HIBERLITE_NVP(seti);
+        ar & HIBERLITE_NVP(vvi);
+        ar & HIBERLITE_NVP(mm);
+    }
+    public:
+        set< pair<int,double> > seti;
+        vector< vector<int> >  vvi;
+        map< vector<string>, map<string,string> > mm;
+        bool operator==(const X& x){
+            return seti==x.seti && vvi==x.vvi && mm==x.mm;
+        }
 };
 
 HIBERLITE_EXPORT_CLASS(A)
@@ -72,112 +72,112 @@ HIBERLITE_EXPORT_CLASS(B)
 HIBERLITE_EXPORT_CLASS(X)
 
 TEST_CASE() {
-	Database db("catch_test.db");
+    Database db("catch_test.db");
 
-	CHECK_NOTHROW( db.registerBeanClass<A>() );
-	CHECK_NOTHROW( db.registerBeanClass<B>() );
+    CHECK_NOTHROW( db.registerBeanClass<A>() );
+    CHECK_NOTHROW( db.registerBeanClass<B>() );
 
-	vector<string> msg=db.checkModel();
-	for(size_t ci=0;ci<msg.size();ci++)
-		CAPTURE (std::string("model check reported: "+msg[ci]));
-	CHECK( msg.size() == 0 );
+    vector<string> msg=db.checkModel();
+    for(size_t ci=0;ci<msg.size();ci++)
+        CAPTURE (std::string("model check reported: "+msg[ci]));
+    CHECK( msg.size() == 0 );
 
-	CHECK_NOTHROW( db.dropModel() );
-	CHECK_NOTHROW( db.createModel() );
+    CHECK_NOTHROW( db.dropModel() );
+    CHECK_NOTHROW( db.createModel() );
 
-	for(unsigned int i=0;i<5;i++){
-		A obj;
-		obj.i=i;
-		char ss[100];
-		sprintf(ss,"obj_num_%d",i);
-		obj.s=ss;
+    for(unsigned int i=0;i<5;i++){
+        A obj;
+        obj.i=i;
+        char ss[100];
+        sprintf(ss,"obj_num_%d",i);
+        obj.s=ss;
 
-		B x;
-		for(unsigned int i=0;i<5;i++){
-			obj.mm[i%3].push_back(i);
-			obj.mm[i%3].push_back(2*i);
-			obj.vi.push_back(make_pair(i,i*i*i));
-			obj.seti.insert(1.0/i);
+        B x;
+        for(unsigned int i=0;i<5;i++){
+            obj.mm[i%3].push_back(i);
+            obj.mm[i%3].push_back(2*i);
+            obj.vi.push_back(make_pair(i,i*i*i));
+            obj.seti.insert(1.0/i);
 
-			string name("01234567890123123123123");
-			x.name="name " + name.substr(i,3);
-			x.weight=sqrt(i);
-			x.height=-10*i;
-			bean_ptr<B> ptr=db.copyBean(x);
-			if (!ptr.get_object())
-				CAPTURE(x.name);
-			CHECK( ptr.get_object() );
-			obj.v.push_back(ptr);
-		}
-		while(obj.v.size()){
-			obj.v[0].destroy();
-			obj.v.erase(obj.v.begin());
-		}
-		bean_ptr<A> a=db.copyBean(obj);
-		if(a.get_id()%10==0)
-			CAPTURE(std::string("save obj with name ")+(*a).s);
-		CHECK( (a.get_id()%10) != 0 );
-	}
+            string name("01234567890123123123123");
+            x.name="name " + name.substr(i,3);
+            x.weight=sqrt(i);
+            x.height=-10*i;
+            bean_ptr<B> ptr=db.copyBean(x);
+            if (!ptr.get_object())
+                CAPTURE(x.name);
+            CHECK( ptr.get_object() );
+            obj.v.push_back(ptr);
+        }
+        while(obj.v.size()){
+            obj.v[0].destroy();
+            obj.v.erase(obj.v.begin());
+        }
+        bean_ptr<A> a=db.copyBean(obj);
+        if(a.get_id()%10==0)
+            CAPTURE(std::string("save obj with name ")+(*a).s);
+        CHECK( (a.get_id()%10) != 0 );
+    }
 }
 
 TEST_CASE() {
-	Database db("catch_test.db");
+    Database db("catch_test.db");
 //	we will not create or drop tables, so model initialization
 //  is not necessary
 //		db.registerBeanClass<A>();
 //		db.registerBeanClass<B>();
 
-	Database otherdb("catch_copy.db");
-	CHECK_NOTHROW( otherdb.registerBeanClass<A>() );
-	CHECK_NOTHROW( otherdb.registerBeanClass<B>() );
+    Database otherdb("catch_copy.db");
+    CHECK_NOTHROW( otherdb.registerBeanClass<A>() );
+    CHECK_NOTHROW( otherdb.registerBeanClass<B>() );
 
-	CHECK_NOTHROW( otherdb.dropModel() );
-	CHECK_NOTHROW( otherdb.createModel() );
+    CHECK_NOTHROW( otherdb.dropModel() );
+    CHECK_NOTHROW( otherdb.createModel() );
 
-	bean_ptr<A> be=db.loadBean<A>(1);
-	CHECK( be.get_object() );
-	A obj2=*be;
-	bean_ptr<A> a2=otherdb.copyBean(obj2);
-	if (!a2.get_object())
-		CAPTURE(std::string("load obj with name ") + (*a2).s);
-	CHECK( a2.get_object() );
+    bean_ptr<A> be=db.loadBean<A>(1);
+    CHECK( be.get_object() );
+    A obj2=*be;
+    bean_ptr<A> a2=otherdb.copyBean(obj2);
+    if (!a2.get_object())
+        CAPTURE(std::string("load obj with name ") + (*a2).s);
+    CHECK( a2.get_object() );
 }
 
 TEST_CASE() {
-	X x;
-	x.vvi.resize(5);
-	map< vector<string>, map<string,string> > mm;
-	vector<string> randstr;
-	unsigned int NS=30;
-	for(unsigned int i=0;i<NS;i++)
-		randstr.push_back(hiberlite::Transformer::toSQLiteValue(rand()%100+i));
+    X x;
+    x.vvi.resize(5);
+    map< vector<string>, map<string,string> > mm;
+    vector<string> randstr;
+    unsigned int NS=30;
+    for(unsigned int i=0;i<NS;i++)
+        randstr.push_back(hiberlite::Transformer::toSQLiteValue(rand()%100+i));
 
-	for(unsigned int i=0;i<100;i++){
-		x.seti.insert( make_pair(i*i,1.0/i+sqrt(i)) );
-		x.vvi[(i*i)%5].push_back(10003%(i+1));
+    for(unsigned int i=0;i<100;i++){
+        x.seti.insert( make_pair(i*i,1.0/i+sqrt(i)) );
+        x.vvi[(i*i)%5].push_back(10003%(i+1));
 
-		vector<string> vs;
-		for(unsigned int i=0;i<5;i++)
-			vs.push_back( randstr[(17+7*i)%NS] );
-		for(unsigned int i=0;i<10;i++)
-			x.mm[vs][ randstr[(19*i)%NS] ]=randstr[(23*i)%NS];
-	}
+        vector<string> vs;
+        for(unsigned int i=0;i<5;i++)
+            vs.push_back( randstr[(17+7*i)%NS] );
+        for(unsigned int i=0;i<10;i++)
+            x.mm[vs][ randstr[(19*i)%NS] ]=randstr[(23*i)%NS];
+    }
 
-	{
-		Database db("catch_t3.db");
-		CHECK_NOTHROW( db.registerBeanClass<X>() );
-		CHECK_NOTHROW( db.dropModel() );
-		CHECK_NOTHROW( db.createModel() );
-		CHECK_NOTHROW( db.copyBean(x) );
-	}
+    {
+        Database db("catch_t3.db");
+        CHECK_NOTHROW( db.registerBeanClass<X>() );
+        CHECK_NOTHROW( db.dropModel() );
+        CHECK_NOTHROW( db.createModel() );
+        CHECK_NOTHROW( db.copyBean(x) );
+    }
 
-	{
-		Database db("catch_t3.db");
-		CHECK_NOTHROW( db.registerBeanClass<X>() );
-		bean_ptr<X> xptr=db.loadBean<X>(1);
-		CAPTURE( "load failed" );
-		CHECK( (*xptr)==x );
-	}
+    {
+        Database db("catch_t3.db");
+        CHECK_NOTHROW( db.registerBeanClass<X>() );
+        bean_ptr<X> xptr=db.loadBean<X>(1);
+        CAPTURE( "load failed" );
+        CHECK( (*xptr)==x );
+    }
 
 }
 
@@ -207,3 +207,69 @@ TEST_CASE("unsigned long long should survive serialization") {
     CHECK(max_new->ull == max.ull);
 }
 
+
+enum class Gender {
+    male, female
+};
+
+struct Person {
+
+    Gender gender_;
+
+    template <class Archive>
+    void hibernate(Archive & ar) {
+        ar & HIBERLITE_NVP(gender_);
+    }
+};
+
+namespace hiberlite {
+    template<class A> void hibernate(A& ar, Gender& value, const unsigned int)
+    {
+        ar & db_atom<Gender>(value);
+    }
+
+    template<> inline std::string db_atom<Gender>::sqliteStorageClass()
+    {
+        return "INTEGER";
+    }
+
+    template<> template<class Stmt, class Arg> void db_atom<Gender>::loadValue(Stmt& res, Arg& arg)
+    {
+        val = static_cast<Gender>(res.get_int(arg));
+    }
+
+    template<> inline void db_atom<Gender>::bindValue(sqlite3_stmt* stmt, int col)
+    {
+        sqlite3_bind_int(stmt, col, static_cast<int>(val));
+    }
+}
+
+HIBERLITE_EXPORT_CLASS(Person);
+
+SCENARIO("custom enum type conversions") {
+    GIVEN("a db") {
+        hiberlite::Database db("custom.db");
+        {
+            WHEN("I add register a type with an enum field") {
+                db.registerBeanClass<Person>();
+                db.dropModel();
+                db.createModel();
+
+                AND_WHEN("I save some entries in the db") {
+                    Person m = { Gender::male };
+                    Person f = { Gender::female };
+
+                    db.copyBean(m);
+                    db.copyBean(f);
+
+                    THEN("The db contains the entries with the correct enum values") {
+                        auto ppl = db.getAllBeans<Person>();
+                        CHECK(ppl.size() == 2);
+                        CHECK(ppl[0]->gender_ == Gender::male);
+                        CHECK(ppl[1]->gender_ == Gender::female);
+                    }
+                }
+            }
+        }
+    }
+}
